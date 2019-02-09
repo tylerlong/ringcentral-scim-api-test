612d14a73660a9735e3a0dc60294a37817df944c
Tyler Long <tyler4long@gmail.com>
2019-02-09 22:35:25 +0800

    dotenv

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import RingCentral from 'ringcentral-js-concise'
 
 (async () => {
-  const rc = new RingCentral('Nm3CXn1BS0iB8YMNj1F-eQ', 'pouI57T8Rl-D_YHLCB8V_gjzomOpOGRI2dNwh3kXVJTw', 'https://platform.devtest.ringcentral.com')
-
+  const rc = new RingCentral(
+    process.env.RINGCENTRAL_CLIENT_ID,
+    process.env.RINGCENTRAL_CLIENT_SECRET,
+    process.env.RINGCENTRAL_SERVER
+  )
   await rc.authorize({
-    username: '15178795131',
-    extension: '',
-    password: '20131019Yang!'
+    username: process.env.RINGCENTRAL_USERNAME,
+    extension: process.env.RINGCENTRAL_EXTENSION,
+    password: process.env.RINGCENTRAL_PASSWORD
   })
   console.log(rc.token())
   await rc.revoke()
